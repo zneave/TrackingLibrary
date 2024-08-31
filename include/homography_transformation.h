fc51b711dc2d9654651a754ab7600b6fc19f75bc
zane neave <zane@marineai.nz>
2024-08-31 12:28:40 +1200

    eigenToCvMat and cvMatToEigen functions added

--- include/homography_transformation.h
+++ include/homography_transformation.h
@@ -3,7 +3,29 @@
 
 #include "coordinates_transformation.h"
 #include <opencv2/opencv.hpp>
+#include <Eigen/Dense>
 #include <iostream>
+#include <tuple>
+
+cv::Mat eigenToCvMat(const Eigen::MatrixXd& eigen_mat) {
+    cv::Mat cv_mat(eigen_mat.rows(), eigen_mat.cols(), CV_64F);
+    for (int i = 0; i < eigen_mat.rows(); i++) {
+        for (int j = 0; j < eigen_mat.cols(); j++) {
+            cv_mat.at<double>(i, j) = eigen_mat(i, j);
+        }
+    }
+    return cv_mat;
+}
+
+Eigen::MatrixXd cvMatToEigen(const cv::Mat& cv_mat) {
+    Eigen::MatrixXd eigen_mat(cv_mat.rows, cv_mat.cols);
+    for (int i = 0; i < cv_mat.rows; i++) {
+        for (int j = 0; j < cv_mat.cols; j++) {
+            eigen_mat(i, j) = cv_mat.at<double>(i, j);
+        }
+    }
+    return eigen_mat;
+}
 
 class HomographyTransformation : public CoordinatesTransformation {
 public:
@@ -33,7 +55,6 @@ private:
     Eigen::Matrix3d inverse_homography_matrix;
 };
 
-// Calculates HomographyTransformation between points using OpenCV's findHomography
 class HomographyTransformationGetter : public TransformationGetter {
 public:
     HomographyTransformationGetter(int method = cv::RANSAC, double ransac_reproj_threshold = 3.0, int max_iters = 2000, double confidence = 0.995, double proportion_points_used_threshold = 0.9)
@@ -45,14 +66,13 @@ public:
             return std::make_tuple(true, nullptr);
         }
 
-        std::vector<cv::Point2f> curr_pts_cv(curr_pts.rows()), prev_pts_cv(prev_pts.rows());
-        cv::eigen2cv(curr_pts, curr_pts_cv);
-        cv::eigen2cv(prev_pts, prev_pts_cv);
+        cv::Mat curr_pts_cv = eigenToCvMat(curr_pts);
+        cv::Mat prev_pts_cv = eigenToCvMat(prev_pts);
 
         cv::Mat inlier_mask;
         cv::Mat homography_matrix_cv = cv::findHomography(prev_pts_cv, curr_pts_cv, method, ransac_reproj_threshold, inlier_mask, max_iters, confidence);
-        Eigen::Matrix3d homography_matrix;
-        cv::cv2eigen(homography_matrix_cv, homography_matrix);
+        
+        Eigen::Matrix3d homography_matrix = cvMatToEigen(homography_matrix_cv);
 
         double proportion_points_used = static_cast<double>(cv::countNonZero(inlier_mask)) / static_cast<double>(inlier_mask.rows);
         bool update_prvs = proportion_points_used < proportion_points_used_threshold;
